638f673118aa073284dd699b7c31ddd9302ce1b7
Hyeon <powergee101@gmail.com>
2020-08-07 02:59:11 +0900

    Add pipeTest.cpp

---
+++ debug/pipeTest.cpp
@@ -0,0 +1,19 @@
+// sampleAlg: BOJ18870
+// [Input]
+// 5
+// 2 4 -10 4 -9
+// [Output]
+// 2 3 0 3 1
+
+#include <iostream>
+#include <unistd.h>
+
+const int READ = 0;
+const int WRITE = 1;
+
+int main()
+{
+    
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
